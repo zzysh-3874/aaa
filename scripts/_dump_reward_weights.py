7f9890ef8cbd4bbb6902015847963e890d035298
zzysh-3874 <[email protected]>
2026-06-08 14:24:28 +0800

    remove tmp reward dump helper

--- scripts/_dump_reward_weights.py
+++
@@ -1,9 +0,0 @@
-import sys, yaml
-d = yaml.unsafe_load(open(sys.argv[1]))
-r = d["rewards"]
-for k, v in r.items():
-    if isinstance(v, dict) and "weight" in v:
-        w = v["weight"]
-        func = v.get("func", "")
-        fname = func.split(":")[-1] if isinstance(func, str) else ""
-        print(f"{k:40s} weight={w:<10} func={fname}")
