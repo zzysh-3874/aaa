8d56ae8f50bbdc1c8fe1a483432096f17325f159
zzysh-3874 <[email protected]>
2026-06-08 14:23:37 +0800

    tmp: reward weight dump helper

---
+++ scripts/_dump_reward_weights.py
@@ -0,0 +1,9 @@
+import sys, yaml
+d = yaml.safe_load(open(sys.argv[1]))
+r = d["rewards"]
+for k, v in r.items():
+    if isinstance(v, dict) and "weight" in v:
+        w = v["weight"]
+        func = v.get("func", "")
+        fname = func.split(":")[-1] if isinstance(func, str) else ""
+        print(f"{k:40s} weight={w:<10} func={fname}")
